194d7be5801d0aefb574e45663c22b7606ce1029
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-12 00:07:03 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Show < ActiveRecord::Base
 
   def self.highest_rating
@@ -6,6 +7,7 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     Show.where("rating = ?", self.highest_rating).first
+    binding.pry
   end
 
   def self.lowest_rating
